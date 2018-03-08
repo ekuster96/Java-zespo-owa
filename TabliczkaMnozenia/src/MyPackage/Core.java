7c0c2e07777d975bf488944b79e37075dc45897c
ekuster96 <32833978+ekuster96@users.noreply.github.com>
2018-03-08 13:48:53 +0100

    Add files via upload

---
+++ TabliczkaMnozenia/src/MyPackage/Core.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package MyPackage;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Student
+ */
+public class Core {
+    //Random generator = new Random();
+    int a,b;
+
+    public Core(int a, int b) {
+        this.a = a;
+        this.b = b;
+        int c = a*b;
+    }
+    
+}
